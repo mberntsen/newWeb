3dcbb37abadd7478da456ad8d09553e5dca3399b
Elmer de Looff <elmer.delooff@gmail.com>
2010-12-22 22:25:13 +0100

    Fixes: headers don't have an iteritems() method, just items() (request). Swapped HG diff and file around on infoboxes in Webline

--- request.py
+++ request.py
@@ -195,7 +195,7 @@ def HeadersIntoEnviron(environ, headers, skip_pre_existing_http=True):
   pre_existing_http = ()
   if skip_pre_existing_http:
     pre_existing_http = set(var[5:] for var in environ if var[:5] == 'HTTP_')
-  for name, value in headers.iteritems():
+  for name, value in headers.items():
     name = name.replace('-', '_').upper()
     if name in environ or name in pre_existing_http:
       continue  # Skip headers we already have in environ
