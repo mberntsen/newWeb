e432b2376b14027bf79eeee4230eeba09d4af8b9
Elmer de Looff <elmer.delooff@gmail.com>
2014-06-15 15:17:20 +0200

    Removed the separate router directory and moved the relevant application-building to the scaffold __init__.

---
+++ uweb/scaffold/base/__init__.py
@@ -0,0 +1,24 @@
+"""A uWeb base project."""
+
+# Third-party modules
+import uweb
+
+# Application
+from . import pages
+
+
+def main():
+  """Creates a newWeb application.
+
+  The application is created from the following components:
+
+  - The presenter class (PageMaker) which implements the request handlers.
+  - The routes iterable, where each 2-tuple defines a url-pattern and a the
+    name of a presenter method which should handle it.
+  - The configuration file (ini format) from which settings should be read.
+  """
+  config = os.path.join(os.path.dirname(__file__), 'config.ini')
+  routes = [
+      ('/', 'Index'),
+      ('/(.*)', 'FourOhFour'))
+  return uweb.NewWeb(pages.PageMaker, routes, config=uweb.ParseConfig(config))
