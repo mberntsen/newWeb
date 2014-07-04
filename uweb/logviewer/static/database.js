a2015bf32ae26d0288ba1b73dba02c356ce4dae1
Elmer de Looff <elmer.delooff@gmail.com>
2014-07-04 17:41:20 +0200

    Removed logviewer project now that this has its own repo.
    This resolves #3

--- uweb/logviewer/static/database.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){
-  jQuery('#events thead').click(function(){
-    $(this).closest('li').toggleClass('closed').toggleClass('open') });
-
-  jQuery('#level').val(jQuery('#prev_level').val());
-  jQuery('#count').val(jQuery('#prev_count').val());
-});
-
